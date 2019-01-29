6005ede16a602055d0f12ab417f92761d9e8bd50
Ekene Akpati <emakpati@gmail.com>
2019-01-28 19:20:52 -0800

    Add create new user func in signup

--- signup.py
+++ signup.py
@@ -14,3 +14,18 @@ driver.get(signup_url)
 i = 0
 user = usr.all_data.USERNAME[i]
 
+def create_user():
+    # Locate elements
+    enter_email = driver.find_element_by_name("emailOrPhone")
+    enter_name = driver.find_element_by_name("fullName")
+
+    enter_username = driver.find_element_by_name("username")
+    enter_psswd = driver.find_element_by_name("password")
+
+    # Send keys
+    enter_email.send_keys(usr.all_data.EMAIL[2])
+    enter_name.send_keys(usr.all_data.NAME[2])
+
+    enter_username.send_keys(usr.all_data.USERNAME[2])
+    enter_psswd.send_keys(usr.key)
+    enter_psswd.send_keys(Keys.RETURN)
